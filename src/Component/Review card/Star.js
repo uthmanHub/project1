890fe09c5b9d7dc00280c66dad42309fdbf34c7a
Uthman <uthmancod@gmail.com>
2023-06-08 12:28:09 +0100

    Updating the star icon on mobile iew to display

--- src/Component/Review card/Star.js
+++ src/Component/Review card/Star.js
@@ -1,10 +1,12 @@
 import React from 'react'
+// import Icon from "./icon-star.svg"
 
 const Star = () => {
   return (
         <div className='icon'>
             <img  src='../icon-star.svg'  alt='star icon' />
             <div className='oval'></div>
+            {/* <img src={Icon} alt='display' /> */}
 
         </div>
     )
